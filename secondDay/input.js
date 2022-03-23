4ade611661230dbc46d6b71e28fa00facfcbe226
Celina Takeda <takedacarina@gmail.com>
2022-03-23 09:29:01 -0300

    commit

--- secondDay/input.js
+++ secondDay/input.js
@@ -1,6 +1,6 @@
 const nome = prompt("Qual o seu nome? ");
 const idade = prompt("Quantos anos você tem? ");
-const linguagem = prompt("Qual lingugem de programação você está estudando?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
 
 const msg = alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
 
