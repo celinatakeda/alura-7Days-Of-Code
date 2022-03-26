51f08296304df647f46bba2dc0e7dab201128a54
Celina Takeda <takedacarina@gmail.com>
2022-03-26 12:32:35 -0300

    commit

---
+++ fifthDay/script.js
@@ -0,0 +1,4 @@
+function Inserir() {
+    var inserirAlimentos = document.getElementById("inserir");
+    inserirAlimentos.innerHTML = "<input type='text' placeholder='Qual comida você deseja inserir?'/>"
+}
